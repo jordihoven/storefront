b6b4a019c9d99075838b05dab2d1419235866652
Jordi Hoven <hovenjordi@gmail.com>
2022-01-21 17:21:17 +0100

    Worked on Cart

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+// TODO: The collection image should be used as Hero Image.
+// TODO: console.log(collections[1].image.src) gets you the correct img
+// TODO: Find a way to give that src as a src for the background image of the html!
+
 //Initialising the client...
 const client = ShopifyBuy.buildClient({
     domain: 'drinkz-development-store.myshopify.com',
     storefrontAccessToken: 'f03245f1af9d31e8b41d045819e8bd40'
 });
 
-//Creating the Product Card variables...
+//Creating the Product Card elements
 let productTitle = document.querySelector(".title");
 let productDescription = document.querySelector(".description");
 let productImage = document.querySelector(".image");
 
 // Fetch all collections, including their products
 client.collection.fetchAllWithProducts().then((collections) => {
+
+    console.log(collections[1].image.src);
+
     //log all products in the specific collection...
     collections[1].products.forEach(element => {
 
@@ -31,31 +38,48 @@ client.collection.fetchAllWithProducts().then((collections) => {
     });
 });
 
-// Create an empty checkout
+// Creating Checkout
 client.checkout.create().then((checkout) => {
-    // Do something with the checkout
-
     console.log(checkout);
     console.log(checkout.subtotalPrice);
     console.log(checkout.webUrl);
     console.log(checkout.id);
-
-    //when add to cart is clicked...
-    document.querySelector('.add-to-cart').onclick = function addToCart() {
-        //add to cart code...
-        const checkoutId = checkout.id; // ID of an existing checkout
-        const lineItemsToAdd = [
-            {
-                variantId: 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MTU4NDI5NTU3NTcxNA==',
-                quantity: 1      
-            }
-        ];
-        
-        // Add an item to the checkout
-        client.checkout.addLineItems(checkoutId, lineItemsToAdd).then((checkout) => {
-        // Do something with the updated checkout
-        console.log(checkout.lineItems); // Array with one additional line item
-        })
-    }
-
+    //Creating public variables, to be used outside the function TODO: Check if this is safe!
+    currentCheckout = checkout;
+    currentCheckoutId = checkout.id;
+    lineItemsToAdd = [
+        {
+            variantId: 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MTU3MzAxNjU2Nzk3MA==', //TODO: make this dynamic...
+            quantity: 1      
+        }
+    ];
+    lineItemIdsToRemove = [
+        'Z2lkOi8vc2hvcGlmeS9DaGVja291dExpbmVJdGVtLzQxNTczMDE2NTY3OTcwMD9jaGVja291dD00Y2Q3OTkwNjA5M2Q3Njc3ZDZmZWViOWExY2E0YjhkMA=='
+    ];
 });
+
+//add to cart
+document.querySelector('.add-to-cart').onclick = function addToCart() {
+    console.log('Checkout id in scope= ' + currentCheckoutId);
+    // Add an item to the checkout
+    client.checkout.addLineItems(currentCheckoutId, lineItemsToAdd).then((checkout) => {
+    // Do something with the updated checkout
+    console.log(checkout.lineItems); // Array with one additional line item
+    console.log(checkout.lineItems[0].id);
+    document.querySelector('.cart-total').innerHTML = checkout.subtotalPrice;
+    })
+}
+
+//remove from cart
+ document.querySelector('.remove-from-cart').onclick = function removeFromCart() {
+    client.checkout.removeLineItems(currentCheckoutId, lineItemIdsToRemove).then((checkout) => {
+    //console.log(checkout.lineItems);
+    document.querySelector('.cart-total').innerHTML = checkout.subtotalPrice;
+    });
+}
+
+//create checkout url
+document.querySelector('.checkout').onclick = function getCheckout() {
+    //console.log(currentCheckout.webUrl);
+    location.href = currentCheckout.webUrl;
+}

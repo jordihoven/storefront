9fe5b86867bc59d6e12a633cff1e74bba22414a9
Jordi Hoven <hovenjordi@gmail.com>
2022-01-25 14:02:56 +0100

    Add fetch entire collection + checkout
    You are now able to fetch all products inside a collection. A dynamic CSS grid is created to house the products. You can also add and remove each product individually to and from the cart!

--- script.js
+++ script.js
@@ -1,82 +1,84 @@
-// TODO: The collection image should be used as Hero Image.
-// TODO: console.log(collections[1].image.src) gets you the correct img
-// TODO: Find a way to give that src as a src for the background image of the html!
-
 //Initialising the client...
 const client = ShopifyBuy.buildClient({
     domain: 'drinkz-development-store.myshopify.com',
     storefrontAccessToken: 'f03245f1af9d31e8b41d045819e8bd40'
 });
 
-//Creating the Product Card elements
-let productTitle = document.querySelector(".title");
-let productDescription = document.querySelector(".description");
-let productImage = document.querySelector(".image");
-
-// Fetch all collections, including their products
-client.collection.fetchAllWithProducts().then((collections) => {
-
-    console.log(collections[1].image.src);
-
-    //log all products in the specific collection...
-    collections[1].products.forEach(element => {
-
-        let titles = element.title;
-        let descriptions = element.description;
-        let images = element.images[0].src;
-
-        console.log(element)
-        console.log(element.title);
-        console.log(element.description);
-        console.log(element.images[0].src); //TODO: write a catch if there is no image. now errors..
-        
-        //sending the values to the DOM...
-        productImage.src = images;
-        productTitle.innerHTML = titles;
-        productDescription.innerHTML = descriptions;
-
-    });
-});
-
 // Creating Checkout
 client.checkout.create().then((checkout) => {
-    console.log(checkout);
-    console.log(checkout.subtotalPrice);
-    console.log(checkout.webUrl);
-    console.log(checkout.id);
-    //Creating public variables, to be used outside the function TODO: Check if this is safe!
     currentCheckout = checkout;
     currentCheckoutId = checkout.id;
-    lineItemsToAdd = [
-        {
-            variantId: 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC80MTU3MzAxNjU2Nzk3MA==', //TODO: make this dynamic...
-            quantity: 1      
+  });
+
+// Fetch all products inside a collection
+client.collection.fetchAllWithProducts().then((collections) => {
+
+    //setting the collection image as background image...
+    document.querySelector('html').style.backgroundImage = 'url('+collections[1].image.src+')';
+    //creating the grid container...
+    const gridContainer = document.querySelector('.grid-container');
+
+    //for every product inside the first collection...
+    collections[1].products.forEach(element => {
+        //console.log(element);
+
+        //set these to the DOM...
+        let gridItem = document.createElement('div');
+        let productImage = document.createElement('img');
+        let productTitle = document.createElement('h3');
+        let productDescription = document.createElement('p');
+        let addToCartBtn = document.createElement('button');
+        let removeFromCartBtn = document.createElement('button');
+        let lineItemId = element.variants[0].id;
+
+        //create the css grid in the DOM...
+        gridContainer.appendChild(gridItem).className = "grid-item";
+        gridItem.appendChild(productImage).className = "grid-image";
+        gridItem.appendChild(productTitle).className = "grid-title";
+        gridItem.appendChild(productDescription).className = "grid-description";
+        gridItem.appendChild(addToCartBtn).className = "grid-addToCart";
+        gridItem.appendChild(removeFromCartBtn).className = "grid-removeFromCart";
+
+        //send the variables to the DOM...
+        productTitle.innerHTML = element.title;
+        productDescription.innerHTML = element.description;
+        productImage.src = element.images[0].src;
+        addToCartBtn.innerHTML = "+ Voeg toe";
+        removeFromCartBtn.innerHTML = "- Verwijder";
+
+        //add to cart
+        addToCartBtn.onclick = function() {
+            //setting the clicked product as lineitem...
+            lineItemsToAdd = [
+                {
+                    variantId: lineItemId,
+                    quantity: 1
+                }
+            ]
+            //adding the lineitem to the cart...
+            client.checkout.addLineItems(currentCheckoutId, lineItemsToAdd).then((checkout) => {
+                document.querySelector('.cart-total').innerHTML = "€" + checkout.subtotalPrice;
+            })
         }
-    ];
-    lineItemIdsToRemove = [
-        'Z2lkOi8vc2hvcGlmeS9DaGVja291dExpbmVJdGVtLzQxNTczMDE2NTY3OTcwMD9jaGVja291dD00Y2Q3OTkwNjA5M2Q3Njc3ZDZmZWViOWExY2E0YjhkMA=='
-    ];
-});
 
-//add to cart
-document.querySelector('.add-to-cart').onclick = function addToCart() {
-    console.log('Checkout id in scope= ' + currentCheckoutId);
-    // Add an item to the checkout
-    client.checkout.addLineItems(currentCheckoutId, lineItemsToAdd).then((checkout) => {
-    // Do something with the updated checkout
-    console.log(checkout.lineItems); // Array with one additional line item
-    console.log(checkout.lineItems[0].id);
-    document.querySelector('.cart-total').innerHTML = checkout.subtotalPrice;
-    })
-}
+        //remove from cart
+        removeFromCartBtn.onclick = function() {
+            //fetching the current checkout
+            client.checkout.fetch(currentCheckoutId).then((checkout) => {
+                console.log(checkout.lineItems[0].id);
+                lineItemsToRemove = checkout.lineItems[0].id;
+            });
 
-//remove from cart
- document.querySelector('.remove-from-cart').onclick = function removeFromCart() {
-    client.checkout.removeLineItems(currentCheckoutId, lineItemIdsToRemove).then((checkout) => {
-    //console.log(checkout.lineItems);
-    document.querySelector('.cart-total').innerHTML = checkout.subtotalPrice;
+            lineItemIdsToRemove = [
+                lineItemsToRemove
+            ]
+            client.checkout.removeLineItems(currentCheckoutId, lineItemIdsToRemove).then((checkout) => {
+                document.querySelector('.cart-total').innerHTML = "€" + checkout.subtotalPrice;
+            });
+        }
+
     });
-}
+});
 
 //create checkout url
 document.querySelector('.checkout').onclick = function getCheckout() {
